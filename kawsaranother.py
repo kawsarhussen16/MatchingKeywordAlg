ecd4f0a40fa770fabb32540bd61a3de34eb76c24
Kawsar Hussen <kawsarhussen@gmail.com>
2019-07-04 13:59:48 -0400

    Basic Naive implemented

---
+++ kawsaranother.py
@@ -0,0 +1,22 @@
+# Basic Naive Algorithm
+
+def BasicNaive(keyword, url): 
+    M = len(keyword) 
+    N = len(url) 
+  
+    # A loop to slide ulr[] one by one 
+    for i in range(N - M + 1): 
+        j = 0
+          
+        # For current index i, check  
+        # for pattern match 
+        for j in range(0, M): 
+            if (url[i + j] != keyword[j]): 
+                break
+  
+        if (j == M - 1):  
+            print("Keyword match at index ", i) 
+  
+url = "targetthisstringtargett"
+keyword = "target"
+BasicNaive(keyword, url) 
